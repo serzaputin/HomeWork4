7040d96286db6f0750c0a79dd6d4ac2335a23dd1
aputin <aputin.serg@yandex.ru>
2022-09-25 19:01:29 +0300

    ДЗ4

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,33 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Привет мир!");
+        System.out.println("Задача 1");
+        int a=100000000;
+        System.out.println(a);
+        byte b=120;
+        System.out.println(b);
+        short c=-11000;
+        System.out.println(c);
+        long d=123000000000L;
+        System.out.println(d);
+        float e=1000;
+        System.out.println(e);
+        double f=120000000;
+        System.out.println(f);
+
+        System.out.println("Задача 2");
+        int t=27897;
+        System.out.println(t);
+        byte y=67;
+        System.out.println(y);
+        short o=-159;
+        System.out.println(o);
+        long q=987678965549L;
+        System.out.println(q);
+        float w=27.12f;
+        System.out.println(w);
+        double r=2.786;
+        System.out.println(r);
 
     }
 }
