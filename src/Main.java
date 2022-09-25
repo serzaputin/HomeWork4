5d413a2e3aa015f272370a7a21bcded14e43ef3f
aputin <aputin.serg@yandex.ru>
2022-09-25 20:03:21 +0300

    ДЗ4

--- src/Main.java
+++ src/Main.java
@@ -29,5 +29,34 @@ public class Main {
         double r=2.786;
         System.out.println(r);
 
+        System.out.println("Задача 3");
+        int ticherOne=23;
+        System.out.println("У Людмилы Павловны  "+ticherOne+" ученика");
+        byte ticherTwo=27;
+        System.out.println("У Анны Сергеевны "+ticherTwo+" учеников");
+        short ticherThree=30;
+        System.out.println("У Екатерины Андреевны "+ticherThree+" учеников");
+        System.out.println("Всего "+(ticherOne+ticherTwo+ticherThree)+" учеников");
+        long paper=480;
+        System.out.println("На каждого ученика расчитано "+(paper/(ticherOne+ticherTwo+ticherThree))+" листов бумаги");
+
+        System.out.println("Задача 4");
+        int bottle=16;
+        int time=2;
+        System.out.println("За "+time+" минуты машина производит "+bottle+" бутылок");
+        byte time1=20;
+        short tame2=1440;
+        int time3=4320;
+        long taim4=43200;
+        System.out.println("за "+time1+" машина произвела "+(bottle*time1)+" бутылок");
+        System.out.println("за "+tame2+" машина произвела "+(bottle*tame2)+" бутылок");
+        System.out.println("за "+time3+" машина произвела "+(time3*bottle)+" бутылок");
+        System.out.println("за "+taim4+" машина произвела "+(taim4*bottle)+" бутылок");
+
+        System.out.println("Задача 5");
+
+
+
+
     }
 }
